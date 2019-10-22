7de858b3348e569cc45402ae62311c9901d2900c
moeko <moeko@192.168.1.2>
2019-10-22 15:24:07 +0900

    update search youtubePage

--- src/components/Api.vue
+++ src/components/Api.vue
@@ -1,44 +1,63 @@
 <template>
   <v-container>
-    <!-- <p>{{result.data.results}}</p>
-    <ul v-for="re in result.data.results" :key="re.trackId">
-      <li>
-        <img :src="re.artworkUrl100" :art="re.trackName"/>
-        {{re.country}}{{re.artistName}}
+    <v-row>
+    <v-col cols="2"></v-col>
+    <v-col cols="7">
+      <v-text-field
+        v-model="keyWord"
+        label="検索"
+        outlined>
+      </v-text-field>
+    </v-col>
+    <v-col cols="3">
+      <v-btn height="55" width="80" @click="searchYoutube(keyWord)"><i class="material-icons">search</i></v-btn>
+    </v-col>
+    </v-row>
+    <ul v-for="(item, index) in result.items" :key="item.title">
+      <li v-if="index === 0">
+        <p>{{item.snippet.channelTitle}}</p>
+        <img :src="item.snippet.thumbnails.default.url"/>
+        <p>説明</p>
+        <p>{{item.snippet.description}}</p>
       </li>
-    </ul> -->
-    <p>{{result.items}}</p>
-    <ul v-for="item in result.items" :key="item.title">
-      <li>{{item.snippet}}</li>
+      <li v-else-if="index > 0">
+        <p>{{item.snippet.title}}</p>
+        <img :src="item.snippet.thumbnails.medium.url"/>
+      </li>
     </ul>
   </v-container>
 </template>
 
 <script>
-// import axios from './../../node_modules/axios'
 import YouTube from './../../node_modules/youtube-node'
 
 const youtube = new YouTube()
-youtube.setKey('取得した自身のapikey')
+youtube.setKey('apikey')
 
 export default {
   data: () => ({
     showResult: false,
-    result: ''
+    result: '',
+    keyWord:''
   }),
   computed: {
   //
 },
   mounted () {
-    this.searchApiResult()
+    this.searchYoutubeResult()
   },
   methods: {
-    async searchApiResult () {
-    //   this.result = await axios.get(`//itunes.apple.com/search?term=""&country=jp&entity=musicVideo`)
-    youtube.search('keyword', 10, (error, result) => {
-      if (error) console.log(error)
+    async searchYoutubeResult (keyWord) {
+      let searchWord = 'vue.js'
+      if (keyWord) searchWord = keyWord
+
+      youtube.search(searchWord, 10, (error, result) => {
+        if (error) console.log(error)
       this.result = result
-    })
+      })
+    },
+    searchYoutube (keyWord) {
+      this.searchYoutubeResult(keyWord)
     }
   }
 };
