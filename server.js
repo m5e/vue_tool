fb53ae2b72da91bedab682b234686407cfe69cca
moeko <moeko@192.168.1.2>
2019-10-24 01:44:56 +0900

    refs webサーバ構築

--- server.js
+++ server.js
@@ -11,9 +11,15 @@ axios.get('http://weather.livedoor.com/forecast/webservice/json/v1?city=400040')
 });
 
 app.get('/', (req, res) => {
-  res.send('hello')
-  console.log('re', re);
+  res.json({location: re.data.location,forecasts:re.data.forecasts, text: re.data.description.text, publicTime:re.data.publicTime, copyright:re.data.copyright});
+  if (!re.data.description.text) {
+
+    res.status(404).json({
+      error: {
+          message: 'Not data found.'
+      }
+    });
+  }
 });
 
-// ポート3000でサーバを立てる
 app.listen(3000, () => console.log('Listening on port 3000'));
