6cd3a77d419a7f554f84e939a128269b5b674b8c
moeko <moeko@192.168.1.2>
2019-10-24 01:51:35 +0900

    refs コメント削除、変数名変更(re→result)

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const axios = require('axios');
 const express = require('express');
 
-// expressアプリを生成する
 const app = express();
 
-let re = '';
+let result = '';
 axios.get('http://weather.livedoor.com/forecast/webservice/json/v1?city=400040')
 .then(response => {
-  re = response;
+  result = response;
 });
 
 app.get('/', (req, res) => {
-  res.json({location: re.data.location,forecasts:re.data.forecasts, text: re.data.description.text, publicTime:re.data.publicTime, copyright:re.data.copyright});
-  if (!re.data.description.text) {
+  res.json({
+    location: result.data.location,
+    forecasts:result.data.forecasts,
+    text: result.data.description.text,
+    publicTime:result.data.publicTime,
+    copyright:result.data.copyright
+  });
+
+  if (!result.data.description.text) {
 
     res.status(404).json({
       error: {
