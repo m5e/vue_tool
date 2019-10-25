f74c5439d150288bb7268ada48f6c40391b53ab1
moeko <m-sju-ke13@softbank.ne.jp>
2019-10-26 03:04:52 +0900

    refs Calendar button implementation

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,15 +1,32 @@
 <template>
   <v-sheet height="500">
+    <v-btn color="blue-grey" text rounded @click="$refs.calendar.prev()">
+      <v-icon large>keyboard_arrow_left</v-icon>
+    </v-btn>
+    <v-btn color="blue-grey" text rounded @click="$refs.calendar.next()">
+      <v-icon large>keyboard_arrow_right</v-icon>
+    </v-btn>
     <v-calendar
       type="month"
+      class="pt-5"
+      ref="calendar"
+      v-model="start"
     ></v-calendar>
   </v-sheet>
 </template>
 
+
 <script>
 export default {
   data: () => ({
-    //
+    //prev(),next() の実行に必要
+    start: '2019-10-01'
   }),
+  mounted () {
+   //
+  },
+  methods: {
+    //
+  }
 };
 </script>
