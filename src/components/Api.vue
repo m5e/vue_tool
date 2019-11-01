1a6b0cd237d7b7db7cc3324ffe4f11914970f426
sasakawa <sasakawa@intra-mart.jp>
2019-11-01 14:20:59 +0900

    Adjust the layout

--- src/components/Api.vue
+++ src/components/Api.vue
@@ -18,12 +18,16 @@
     </v-row>
     <ul v-for="(item, index) in result.items" :key="item.title">
       <li v-if="index === 0">
-        <p>{{item.snippet.channelTitle}}</p>
-        <img :src="item.snippet.thumbnails.default.url"/>
-        <p>説明</p>
-        <p>{{item.snippet.description}}</p>
+        <v-card outlined color="grey lighten-3">
+          <p class="title ml-5 font-weight-black">{{item.snippet.channelTitle}}</p>
+          <v-avatar class="ml-5" size="100">
+            <v-img :src="item.snippet.thumbnails.default.url"/>
+          </v-avatar>
+          <p class="title ml-5">説明</p>
+          <p class="ml-5">{{item.snippet.description}}</p>
+        </v-card>
       </li>
-      <li v-else-if="index > 0">
+      <li v-else-if="index > 0" class="pb-9 font-weight-black">
         <p>{{item.snippet.title}}</p>
         <iframe
           width="700"
@@ -75,3 +79,9 @@ export default {
   }
 };
 </script>
+
+<style>
+li {
+  list-style: none;
+}
+</style>
