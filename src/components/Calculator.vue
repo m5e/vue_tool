2524641a2d199edba45585de4cc83d2a122382c8
moeko <m-sju-ke13@softbank.ne.jp>
2019-11-07 00:37:27 +0900

    途中経過

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -28,9 +28,9 @@
 export default {
   data: () => ({
     // 表示用の変数
-    viewNum: "",
+    viewNum: "0",
     // 計算用の変数
-    tmpNum: [],
+    tmpNum: ["0"],
     items: [
       ["7", "8", "9", "÷"],
       ["4", "5", "6", "×"],
@@ -41,7 +41,7 @@ export default {
     isOutputed: false,
     // 直前に入力された演算子の種類
     isOperator: "",
-    // 直前に演算子が入力されているか否かを確認するフラグ
+    // 演算子の入力を確認
     isOperatorFlg: false,
     textFieldStyle: {
       "background-color": "azure"
@@ -52,92 +52,131 @@ export default {
       const item = itemNum;
 
       if (item === "AC") {
-        this.viewNum = "";
-        this.tmpNum = [];
+        this.viewNum = "0";
+        this.tmpNum.length = 0;
+        this.tmpNum.push("0");
+        this.isOperator = "";
+        this.isOperatorFlg = false;
 
         return;
-      }
+      } else if (item === "+" || item === "-" || item === "×" || item === "÷") {
+        if (this.tmpNum.length === 1) {
+          if (item === "0") return;
 
-      if (this.tmpNum.length === 0) {
-        if (
-          item === "+" ||
-          item === "-" ||
-          item === "×" ||
-          item === "÷" ||
-          item === "="
-        ) {
+          this.tmpNum.push(item);
+          this.viewNum = this.tmpNum.join("");
+          this.isOperator = item;
+          this.isOperatorFlg = true;
+          this.isOutputed = false;
+
           return;
-        }
-
-        this.tmpNum.push(item);
-        this.viewNum = this.tmpNum.join("");
-      } else if (this.tmpNum.length > 0) {
-        if (item === "+" || item === "-" || item === "×" || item === "÷") {
+        } else if (this.tmpNum.length > 1) {
           if (!this.isOperatorFlg) {
             this.isOperator = item;
             this.isOperatorFlg = true;
+            this.isOutputed = false;
 
             this.tmpNum.push(item);
             this.viewNum = this.tmpNum.join("");
-
             return;
           } else if (this.isOperatorFlg && item === this.isOperator) {
             return;
           } else if (this.isOperatorFlg && item !== this.isOperator) {
             this.isOperator = item;
+            this.isOutputed = false;
+            if (
+              this.tmpNum[this.tmpNum.length - 1] === "+" ||
+              this.tmpNum[this.tmpNum.length - 1] === "-" ||
+              this.tmpNum[this.tmpNum.length - 1] === "×" ||
+              this.tmpNum[this.tmpNum.length - 1] === "÷"
+            ) {
+              this.tmpNum[this.tmpNum.length - 1] = item;
+              this.viewNum = this.tmpNum.join("");
 
-            this.tmpNum[this.tmpNum.length - 1] = item;
-            this.viewNum = this.tmpNum.join("");
-
-            return;
-          }
-        }
-
-        if (item === "=") {
-          if (!this.isOutputed) {
-            this.viewNum = String(
-              this.tmpNum
-                .join("")
-                .replace(/×/, "*")
-                .replace(/÷/, "/")
-            );
-
-            this.viewNum = new Function("return " + this.viewNum)();
-
-            // 初期化
-            this.tmpNum.length = 0;
-
-            this.tmpNum[0] = this.viewNum;
-
-            this.isOutputed = true;
-            this.isOperator = "";
-            this.isOperatorFlg = false;
-          }
-
-          return;
-        } else if (this.tmpNum[this.tmpNum.length - 1] === "0") {
-          if (
-            item === "+" ||
-            item === "-" ||
-            item === "×" ||
-            item === "÷" ||
-            item === "="
-          ) {
+              return;
+            }
             this.tmpNum.push(item);
             this.viewNum = this.tmpNum.join("");
 
             return;
           }
+        }
+      } else if (item === "=") {
+        if (!this.isOutputed) {
+          this.tmpNum.forEach((num, index) => {
+            if (num === "×") this.tmpNum[index] = "*";
+            if (num === "÷") this.tmpNum[index] = "/";
+          });
 
-          this.tmpNum[0] = item;
-          this.viewNum = this.tmpNum[0];
+          if (
+            this.tmpNum[this.tmpNum.length - 1] === "+" ||
+            this.tmpNum[this.tmpNum.length - 1] === "-" ||
+            this.tmpNum[this.tmpNum.length - 1] === "*" ||
+            this.tmpNum[this.tmpNum.length - 1] === "/"
+          ) {
+            this.tmpNum.pop();
+          }
+
+          this.viewNum = this.tmpNum.join("");
+
+          if (
+            this.tmpNum[this.tmpNum.length - 1] === "+" ||
+            this.tmpNum[this.tmpNum.length - 1] === "-" ||
+            this.tmpNum[this.tmpNum.length - 1] === "*" ||
+            this.tmpNum[this.tmpNum.length - 1] === "/"
+          ) {
+            this.tmpNum.splice(this.tmpNum.length - 1, 1);
+          }
+
+          this.viewNum = new Function("return " + this.viewNum)();
+
+          // 初期化
+          this.tmpNum.length = 0;
+          // this.tmpNum.push(this.viewNum);
+          if (0 < this.tmpNum < 10) {
+            this.viewNum = String(this.viewNum);
+            this.tmpNum[0] = this.viewNum;
+          } else if (this.viewNum >= 10) {
+            // TODO:分割して格納
+          } else if (this.viewNum < 0) {
+            // TODO:演算子と切り分けて格納
+          }
+
+          this.isOutputed = true;
+          this.isOperator = "";
+          this.isOperatorFlg = false;
+        }
+
+        return;
+      } else {
+        if (this.tmpNum.length == 1) {
+          if (this.tmpNum[0] === "0") {
+            if (item === "0") return;
+
+            this.isOutputed = false;
+            this.isOperator = "";
+
+            this.tmpNum[0] = item;
+            this.viewNum = item;
+
+            return;
+          }
+
           this.isOutputed = false;
+          this.isOperator = "";
 
-          return;
-        } else {
           this.tmpNum.push(item);
           this.viewNum = this.tmpNum.join("");
+
+          return;
+        } else if (this.tmpNum.length > 1) {
           this.isOutputed = false;
+          this.isOperator = "";
+
+          this.tmpNum.push(item);
+          this.viewNum = this.tmpNum.join("");
+
+          return;
         }
       }
     }
