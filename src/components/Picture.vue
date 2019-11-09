698c937daead10ff00e32131722d0e262c814aba
moeko <m-sju-ke13@softbank.ne.jp>
2019-11-09 09:03:32 +0900

    Changing the image page layout

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -2,26 +2,31 @@
   <v-layout>
     <v-flex>
       <v-container grid-list-md>
-        <a
+        <!-- <a
           v-if="isShowAnchor"
           :href="targetImage"
           target="_blank"
           @click="getDownloadTarget"
-        >選択した画像を別タブで表示</a>
+        >選択した画像を別タブで表示</a>-->
         <v-layout row wrap>
           <v-flex v-for="(n, index) in 12" :key="n" md3 :class="{active: selectedImage === index}">
-            <v-img
-              :src="`https://picsum.photos/500/300?image=${n * imageNumber + 10}`"
-              :lazy-src="`https://picsum.photos/10/6?image=${n * imageNumber + 10}`"
-              aspect-ratio="1"
-              @click="chackImageStatus(index)"
-            >
-              <template v-slot:placeholder>
-                <v-layout fill-height align-center justify-center ma-0>
-                  <v-progress-circular indeterminate color="grey lighten-5" />
-                </v-layout>
-              </template>
-            </v-img>
+            <v-card min-height="280px" @click="chackImageStatus(index)" color="#F5F5F5	">
+              <v-img
+                :src="`https://picsum.photos/500/300?image=${n * imageNumber + 10}`"
+                :lazy-src="`https://picsum.photos/10/6?image=${n * imageNumber + 10}`"
+                aspect-ratio="1"
+              >
+                <template v-slot:placeholder>
+                  <v-layout fill-height align-center justify-center ma-0>
+                    <v-progress-circular indeterminate color="grey lighten-5" />
+                  </v-layout>
+                </template>
+              </v-img>
+              <v-chip class="mt-2 ml-1" :color="keyword[index].color" outlined>
+                <i class="material-icons mr-1">{{keyword[index].icon}}</i>
+                <span>{{keyword[index].text}}</span>
+              </v-chip>
+            </v-card>
           </v-flex>
         </v-layout>
       </v-container>
@@ -40,7 +45,22 @@ export default {
     // 選択した画像のURL
     targetImage: "",
     // 選択した画像のURLに遷移するための a タグの表示/非表示を制御
-    isShowAnchor: false
+    isShowAnchor: false,
+    // chip component
+    keyword: [
+      { text: "favorite", icon: "loyalty", color: "#DC143C" },
+      { text: "travel", icon: "airplanemode_active", color: "#0000CD" },
+      { text: "fun", icon: "insert_emoticon", color: "orange" },
+      { text: "Going out", icon: "directions_walk", color: "#2E8B57" },
+      { text: "cycling", icon: "directions_bike", color: "#00BFFF" },
+      { text: "Parking area", icon: "local_parking", color: "#32CD32" },
+      { text: "train", icon: "tram", color: "#9370DB" },
+      { text: "work", icon: "home_work", color: "#FFCC00" },
+      { text: "Break time", icon: "free_breakfast", color: "#CD853F" },
+      { text: "shop", icon: "storefront", color: "#FF4F50" },
+      { text: "out side", icon: "emoji_nature", color: "#EE82EE" },
+      { text: "like", icon: "thumb_up_alt", color: "#4169E1" }
+    ]
   }),
   mounted() {
     // 画像をランダムで表示
@@ -72,6 +92,7 @@ export default {
       const strexp = /https:\/\/[a-z]*\.[a-z]*\/[0-9]*\/[0-9]*\?image=[0-9]*/;
       // 画像のURL取得
       this.targetImage = cssText.match(strexp)[0];
+      console.log(this.targetImage);
     }
   }
 };
