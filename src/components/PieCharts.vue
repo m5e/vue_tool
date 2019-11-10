506f545b9191abbb408bc37cda6d740e903c81bb
moeko <m-sju-ke13@softbank.ne.jp>
2019-11-11 02:32:12 +0900

    Add PieChart Page

---
+++ src/components/PieCharts.vue
@@ -0,0 +1,51 @@
+<template>
+  <highcharts :options="chartOptions" :updateArgs="updateArgs"></highcharts>
+</template>
+
+<script>
+import Highcharts from "highcharts";
+import exportingInit from "highcharts/modules/exporting";
+
+exportingInit(Highcharts);
+
+export default {
+  props: ["data", "title"],
+  data() {
+    return {
+      chartOptions: {
+        chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false,
+          type: "pie"
+        },
+        exporting: {
+          enabled: false
+        },
+        title: {
+          text: ""
+        },
+        series: [
+          {
+            data: [
+              {
+                name: "test",
+                y: 29.9
+              },
+              {
+                name: "test2",
+                y: 29.9
+              },
+              {
+                name: "test3",
+                y: 29.94
+              }
+            ]
+          }
+        ]
+      },
+      updateArgs: [true, true, { duration: 1000 }]
+    };
+  }
+};
+</script>
