4bb9d867e241f43cb591ec85e00a5bfa8a5445d8
moeko <m-sju-ke13@softbank.ne.jp>
2019-11-12 02:17:29 +0900

    Add BarChart

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -3,15 +3,34 @@
     <v-flex>
       <LineGraph />
       <v-divider class="mt-8 mb-8" />
+      <BarCharts />
+      <v-divider class="mt-8 mb-8" />
       <PieCharts />
     </v-flex>
+    <v-slide-y-transition>
+      <v-btn
+        v-show="isShowScrollButton"
+        v-scroll="handleScrollButton"
+        fab
+        fixed
+        bottom
+        right
+        color="teal accent-4"
+        @click="goTopPage"
+      >
+        <i class="material-icons">arrow_upward</i>
+      </v-btn>
+    </v-slide-y-transition>
   </v-layout>
 </template>
 <script>
+import BarCharts from "./BarCharts";
+import common from "./../util/common";
 import LineGraph from "./LineGraph";
 import PieCharts from "./PieCharts";
 
 export default {
+  mixins: [common],
   data() {
     return {
       //
@@ -19,7 +38,8 @@ export default {
   },
   components: {
     LineGraph,
-    PieCharts
+    PieCharts,
+    BarCharts
   }
 };
 </script>
