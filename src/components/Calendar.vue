d6a2a6291e5d446f3ab3987888150b91ddfc9d65
moeko <m-sju-ke13@softbank.ne.jp>
2019-11-17 22:34:25 +0900

    Added process to add appointment

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -8,19 +8,43 @@
     </v-btn>
     <v-calendar
       ref="calendar"
-      v-model="start"
       type="month"
+      v-model="start"
       class="pt-5"
-      color="primary"
       :events="holidaysList"
       :event-color="getHolidayColor"
-      @click:date="clickDate"
+      color="primary"
+      @click:date="clickedDate"
     />
-    <v-dialog v-model="isShowDateDialog" max-width="330px">
-      <v-card>
+    <v-dialog v-model="isShowDateDialog" max-width="350px">
+      <v-card height="210px">
+        <v-toolbar flat color="primary">
+          <v-row>
+            <v-toolbar-title>
+              <v-btn text rounded @click="switchInputLimit(false)">
+                <i class="material-icons">create</i>
+              </v-btn>
+            </v-toolbar-title>
+
+            <v-spacer />
+
+            <v-toolbar-items>
+              <v-btn @click="switchInputLimit(true)" rounded text>
+                <i class="material-icons">clear</i>
+              </v-btn>
+            </v-toolbar-items>
+          </v-row>
+        </v-toolbar>
         <v-card-title>{{ selectedDate }}</v-card-title>
+        <v-text-field
+          label="Outlined"
+          outlined
+          :filled="isFilled"
+          :readonly="isReadonly"
+          @keydown.enter="addHolidayList"
+          v-model="plan"
+        ></v-text-field>
         <v-spacer />
-        <v-btn @click="isShowDateDialog = false" outlined color="green">close</v-btn>
       </v-card>
     </v-dialog>
   </v-sheet>
@@ -29,12 +53,15 @@
 <script>
 export default {
   data: () => ({
-    //prev(),next() の実行に必要
+    // prev(),next() の実行に必要
     start: null,
     end: null,
     holidaysList: [],
     selectedDate: "",
-    isShowDateDialog: false
+    isShowDateDialog: false,
+    isReadonly: true,
+    isFilled: true,
+    plan: ""
   }),
 
   mounted() {
@@ -51,23 +78,40 @@ export default {
       this.start = year + "-" + month + "-" + date;
     },
     getHolidays() {
-      const tmpHolidaysList = require("./../json/holidays.json"); // TODO：API で取得する実装
+      const tmpHolidaysList = require("./../json/holidays.json"); // TODO：Process obtained by API
       tmpHolidaysList.forEach(data => {
         this.holidaysList.push({
           name: data.text,
           start: data.date,
           end: data.date,
+          detail: null,
           color: "teal"
         });
       });
     },
-    // 祝日の色を指定
     getHolidayColor(event) {
       return event.color;
     },
-    clickDate() {
+    clickedDate() {
       this.selectedDate = this.start;
       this.isShowDateDialog = true;
+    },
+    switchInputLimit(limit) {
+      this.isReadonly = limit;
+      this.isFilled = limit;
+    },
+    addHolidayList() {
+      this.holidaysList.push({
+        name: this.plan,
+        start: this.start,
+        end: this.start,
+        detail: null,
+        color: "primary"
+      });
+
+      // ダイアログを閉じる
+      this.isShowDateDialog = false;
+      this.switchInputLimit(true);
     }
   }
 };
